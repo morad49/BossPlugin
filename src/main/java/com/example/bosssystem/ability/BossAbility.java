16155968e46f97cb14b35e94c0d3e5a0002878d2
dirtmc <moradmc461@gmail.com>
2026-09-01 00:23:41 +0300

    Add BossAbility interface for boss abilities

---
+++ src/main/java/com/example/bosssystem/ability/BossAbility.java
@@ -0,0 +1,10 @@
+package com.example.bosssystem.ability;
+
+import com.example.bosssystem.boss.BossInstance;
+import org.bukkit.entity.Player;
+
+public interface BossAbility {
+    String getName();
+    int getCooldownSeconds();
+    boolean execute(BossInstance boss, Player target);
+}
