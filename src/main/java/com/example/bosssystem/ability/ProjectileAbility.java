44f0fc856d81e53e16b8385d1916acae64f41cc3
dirtmc <moradmc461@gmail.com>
2026-09-01 01:09:56 +0300

    Update ProjectileAbility.java

--- src/main/java/com/example/bosssystem/ability/ProjectileAbility.java
+++ src/main/java/com/example/bosssystem/ability/ProjectileAbility.java
@@ -1,43 +1,28 @@
-package com.bosssystem.ability;
+package com.example.bosssystem.ability;
 
-import com.bosssystem.BossSystem;
-import com.bosssystem.boss.BossInstance;
+import com.example.bosssystem.BossSystem;
+import com.example.bosssystem.boss.BossInstance;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
-public class ProjectileAbility extends BossAbility {
-    public ProjectileAbility(BossSystem plugin) { super(plugin, "projectile"); }
+public class ProjectileAbility {
 
-    @Override
-    public void execute(BossInstance boss, LivingEntity target) {
-        Location current = boss.getEntity().getEyeLocation();
-        double dmg = plugin.getConfigManager().getAbilityDamage(boss.getLevel(), "projectile");
-        
-        new BukkitRunnable() {
-            int ticks = 0;
-            @Override
-            public void run() {
-                if (ticks++ > 80 || !boss.isAlive()) { cancel(); return; }
-                // Homing logic (very slight adjustment)
-                Vector dir = target.getLocation().add(0, 1, 0).toVector().subtract(current.toVector()).normalize().multiply(0.8);
-                current.add(dir);
-                
-                current.getWorld().spawnParticle(Particle.REDSTONE, current, 3, 0.1, 0.1, 0.1, new Particle.REDSTONEOptions(Color.RED, 1.5f));
-                
-                for (Entity e : current.getWorld().getNearbyEntities(current, 0.8, 0.8, 0.8)) {
-                    if (e instanceof Player p) {
-                        p.damage(dmg, boss.getEntity());
-                        cancel();
-                        return;
-                    }
-                }
-            }
-        }.runTaskTimer(plugin, 0, 1);
+    private final BossSystem plugin;
+
+    public ProjectileAbility(BossSystem plugin) {
+        this.plugin = plugin;
+    }
+
+    public void execute(BossInstance boss, Player target) {
+        if (boss == null || target == null) return;
+
+        Location loc = target.getLocation();
+        Particle.DustOptions dustOptions = new Particle.DustOptions(Color.RED, 1.2f);
+
+        if (loc.getWorld() != null) {
+            loc.getWorld().spawnParticle(Particle.DUST, loc.clone().add(0, 1, 0), 20, 0.5, 0.5, 0.5, dustOptions);
+        }
     }
 }
