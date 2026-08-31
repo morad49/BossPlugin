f79fe16058b62a9a8a5d92c27632fa30df47438a
dirtmc <moradmc461@gmail.com>
2026-09-01 01:19:31 +0300

    Update BossAbility.java

--- src/main/java/com/example/bosssystem/ability/BossAbility.java
+++ src/main/java/com/example/bosssystem/ability/BossAbility.java
@@ -4,7 +4,13 @@ import com.example.bosssystem.boss.BossInstance;
 import org.bukkit.entity.Player;
 
 public interface BossAbility {
-    String getName();
-    int getCooldownSeconds();
+    
+    /**
+     * تنفيذ القدرة الخاصة بالبوس
+     * 
+     * @param boss كائن البوس الذي ينفذ القدرة
+     * @param target اللاعب المستهدف
+     * @return true إذا تم تنفيذ القدرة بنجاح، و false إذا فشلت
+     */
     boolean execute(BossInstance boss, Player target);
 }
